224c1086b15e348240e8c3b4d338f2db89ce7cd6
Scott Kao <scottxxx666@gmail.com>
2019-11-14 12:32:18 +0800

    Extract response funciton

--- handler.js
+++ handler.js
@@ -1,18 +1,20 @@
 const list = async function (event) {
-  return {
+  return response({
     statusCode: 200,
     body: 'Hello!',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-  };
+  });
 };
 
 const get = async function (event) {
-  return {
+  return response({
     statusCode: 200,
     body: 'Hello!',
+  });
+};
+
+const response = async function (res) {
+  return {
+    ...res,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
